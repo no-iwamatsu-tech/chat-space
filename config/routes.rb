d36907a729407572dcca62384737bae37685f9dd
Noriki Iwamatsu <no.iwamatsu@gmail.com>
2018-01-29 23:00:55 +0900

    fix and add routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  root "groups#index"
 
   resource :user, only: [:edit, :update]
+  resources :groups, except: [:index, :show, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
