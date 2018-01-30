96900b059db454d06e6533dc0e7d5ccfc2c3cf30
Noriki Iwamatsu <no.iwamatsu@gmail.com>
2018-01-30 23:18:36 +0900

    add group model validate.

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,11 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+  validate :users_no_empty
+
+  def users_no_empty
+    if users.empty?
+      errors.add("Users", "が設定されていません。")
+    end
+  end
 end
