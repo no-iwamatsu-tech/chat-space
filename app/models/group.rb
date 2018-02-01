35126e7e846450bbf11aebbfb097bcc3e10075b5
Noriki Iwamatsu <no.iwamatsu@gmail.com>
2018-02-01 12:57:24 +0900

    delete group validate users_no_empty.

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,11 +4,4 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
-  validate :users_no_empty
-
-  def users_no_empty
-    if users.empty?
-      errors.add("Users", "が設定されていません。")
-    end
-  end
 end
