4ecf702f73d60811e6f8de2028e2e1b3bb9a5760
Noriki Iwamatsu <no.iwamatsu@gmail.com>
2018-02-04 00:48:16 +0900

    create messages create rspec.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -35,8 +35,70 @@ describe MessagesController do
 
     context "not login" do
 
-      it 'redirects to new_user_session_path.' do
+      it "redirects to new_user_session_path." do
         get :index, params: { group_id: group.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "POST #create" do
+
+    context "login" do
+      before do
+        login_user user
+      end
+
+      context "can save" do
+        let(:params) {{
+          group_id: group.id,
+          user_id:  user.id,
+          message:  attributes_for(:message)
+        }}
+
+        it "save message." do
+          expect {
+            post :create,
+            params: params
+          }.to change(Message, :count).by(1)
+        end
+
+        it "redirects to group_messages_path" do
+          post :create, params: params
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context "cannot save" do
+        let(:params) {{
+          group_id: group.id,
+          user_id:  user.id,
+          message:  attributes_for(:message, comment: nil, image: nil)
+        }}
+
+        it "not save message." do
+          expect {
+            post :create,
+            params: params
+          }.not_to change(Message, :count)
+        end
+
+        it "redirects to group_messages_path" do
+          post :create, params: params
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+    end
+
+    context "not login" do
+      let(:params) {{
+        group_id: group.id,
+        user_id:  user.id,
+        message:  attributes_for(:message)
+      }}
+
+      it "redirects to new_user_session_path." do
+        post :create, params: params
         expect(response).to redirect_to(new_user_session_path)
       end
     end
